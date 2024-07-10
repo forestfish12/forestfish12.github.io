e5071d43e9aee522557492a34d3d9b59a24f0930
Forest Fish <forest_fish@yahoo.com>
2024-07-10 16:11:16 -0700

    Finished Initial testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    NavComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
