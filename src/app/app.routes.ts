3fd46a0435d3a70272ffa9eb97d08e8cb898cad0
Forest Fish <forest_fish@yahoo.com>
2024-07-10 16:13:33 -0700

    Deleted old project to start from scratch

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'contact', component: ContactComponent},
-];
