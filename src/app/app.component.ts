b467a561968e7e7e8c107d05d42aa63371f88ced
Forest Fish <forest_fish@yahoo.com>
2024-07-16 17:00:33 -0700

    testing materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatSlideToggleModule
-  ],
+    MatSlideToggleModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    NavComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
